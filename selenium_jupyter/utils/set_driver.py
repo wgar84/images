8790223f8e1db7688458fbd68a203891bbb143b0
Guilherme Garcia <guilherme.garcia@grupofolha.com.br>
2021-12-15 20:15:32 -0300

    still with selenium issues

--- selenium_jupyter/utils/set_driver.py
+++ selenium_jupyter/utils/set_driver.py
@@ -2,8 +2,11 @@
 from selenium import webdriver
 
 
-def set_driver():
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')  # example
+def set_driver(headless=True):
+    options = webdriver.ChromeOptions()
+    if headless:
+        options.add_argument('--headless')  # example
+    options.add_argument("window-size=1400,600")
     driver = webdriver.Remote("http://driver:4444/wd/hub", options=options)
+    driver.maximize_window()
     return driver
