85fbc2a499d4ec463f289e81f4fc51fa74741279
Guilherme Garcia <guilherme.garcia@grupofolha.com.br>
2021-12-16 16:01:43 -0300

    dockerfile experiments

--- selenium_jupyter/utils/set_driver.py
+++ selenium_jupyter/utils/set_driver.py
@@ -4,9 +4,14 @@ from selenium import webdriver
 
 def set_driver(headless=True):
     options = webdriver.ChromeOptions()
+
     if headless:
         options.add_argument('--headless')  # example
+
     options.add_argument("window-size=1400,600")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-setuid-sandbox")
+
     driver = webdriver.Remote("http://driver:4444/wd/hub", options=options)
     driver.maximize_window()
     return driver
